dfb1cba43489e866a7ef3f867be03931fcdc8d0c
thunderawesome <troyjsanders@gmail.com>
2023-02-16 14:00:01 -0600

    Added colors & pricing to cubes

--- src/Components/Quote.tsx
+++ src/Components/Quote.tsx
@@ -1,16 +1,22 @@
 import useQuote from '../Hooks/useQuote'
 import React, { useState } from 'react'
 import { Text } from '@react-three/drei'
+import * as THREE from 'three'
 
 interface Props {
   id: number
   symbol: string
 }
 
+const truncateByDecimalPlace = (value: string, numDecimalPlaces: number) =>
+  Math.trunc(Number(value) * Math.pow(10, numDecimalPlaces)) / Math.pow(10, numDecimalPlaces)
+
 const QuoteRef = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
   const { id, symbol } = props
   const { quote, symbolMap, error, isLoading } = useQuote(symbol)
   const [text, setText] = useState('No quote Available')
+
+  const col = id % 2 === 0 ? new THREE.Color('rgb(200, 0, 0)') : new THREE.Color('rgb(0, 200, 0)')
 
   React.useEffect(() => {
     if (isLoading) {
@@ -24,16 +30,18 @@ const QuoteRef = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
     }
 
     if (symbol && quote) {
-      setText(symbol)
+      const price = truncateByDecimalPlace(quote.price, 2)
+      setText(symbol + '\n\n' + price)
     } else {
       if (symbolMap) {
-        setText(symbolMap[id]?.symbol)
+        const price = truncateByDecimalPlace(symbolMap[id]?.price, 2)
+        setText(symbolMap[id]?.symbol + '\n\n' + price)
       }
     }
   }, [symbol, quote, id, symbolMap, isLoading, text, error])
 
   return (
-    <Text ref={ref} fontSize={0.25} position={[0, 0, 1.25]} color="black">
+    <Text ref={ref} color={col} outlineColor={'silver'} outlineWidth={0.025} textAlign={'center'} fontSize={0.25} position={[0, 0, 1.25]}>
       {text}
     </Text>
   )
